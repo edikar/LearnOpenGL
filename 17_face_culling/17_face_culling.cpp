53e0b4ed0374584520f564d7a1d73bfea0a97fa0
Manul Manul <edikar666@gmail.com>
2020-09-29 22:01:00 +0300

    updated face culling lesson with perspective projection, because it's easier to differentiate the outside and inside of the qube than in orthographic

--- 17_face_culling/17_face_culling.cpp
+++ 17_face_culling/17_face_culling.cpp
@@ -7,17 +7,21 @@
 #include <math.h>
 #include "shader_class.h"
 #include "utils.h"
+#include "camera.h"
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/ext/matrix_clip_space.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 using namespace std;
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 STATUS init();
 void showFrameRate();
@@ -27,9 +31,19 @@ unsigned int loadTexture(char const * path);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-int front_or_back = 0; //start with front
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
 
 GLFWwindow* window;
+
+int front_or_back = 1; //draw front faces first
 
 int main()
 {
@@ -115,6 +129,7 @@ int main()
     glEnable(GL_DEPTH_TEST);
 
     glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CW);
     //glCullFace(GL_FRONT); //
     glCullFace(GL_BACK);
 
@@ -122,9 +137,17 @@ int main()
     //create texture 
     unsigned int texture0 = loadTexture("../images/metal.png");
 
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // per-frame time logic
+        // --------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
 
-	while(!glfwWindowShouldClose(window))
-	{        // input
+        // input
         // -----
         processInput(window);
 
@@ -134,29 +157,40 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         //transform and rotate by time
-        glm::mat4 trans = glm::mat4(1.0f);
-		
-		trans = glm::rotate(trans, -0.5f, glm::vec3(1.0f, 0.0f, 0.0f));
-		trans = glm::rotate(trans, -(float)glfwGetTime()/2, glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 model = glm::rotate(glm::mat4(1.0), glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        model = glm::rotate(model, (float)glm::radians(glfwGetTime()) * 5, glm::vec3(0.0f, 1.0f, 0.0f));
+        glm::mat4 view = camera.GetViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
         ourShader.use();
+        // set the model, view and projection matrix uniforms
+        ourShader.setMat4("model", model);
+        ourShader.setMat4("view", view);
+        ourShader.setMat4("projection", projection);
+
 		ourShader.setInt("texture0", 0);
-		ourShader.setMat4("transform", trans);
 		ourShader.setInt("front_or_back", front_or_back);
 
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
 
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
         glfwPollEvents();
+    }
 
-        showFrameRate();
-	}
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
-	return 0;
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
 }
 
 void showFrameRate(){
@@ -200,7 +234,10 @@ STATUS init()
 	
 	glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback); 
 
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);  
 
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
@@ -219,15 +256,24 @@ STATUS init()
 // ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window)
 {
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
     if(glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS){
         glCullFace(GL_BACK);
-        front_or_back = 0;
+        front_or_back = 1;
     }
     if(glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS){
         glCullFace(GL_FRONT);
-        front_or_back = 1;
+        front_or_back = 0;
     }
 }
 
@@ -238,6 +284,34 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
 }
 
 
